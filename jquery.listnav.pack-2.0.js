5879b22fc36632f5f60c02f70539a7bfca811ccc
Mark Brown <mark@ihwy.com>
2013-06-06 09:09:44 -0700

    Version 2.0
    - added number navigation link
    - added cookie handling
    - added onClick callback method
    - modified CSS internal names
    - removed CSS-name related plugin options

---
+++ jquery.listnav.pack-2.0.js
@@ -0,0 +1,18 @@
+﻿/*
+*
+* jQuery listnav plugin
+* Copyright (c) 2009 iHwy, Inc.
+* Author: Jack Killpatrick
+*
+* Version 2.0 (03/02/2009)
+* Requires jQuery 1.3.2, jquery 1.2.6 or jquery 1.2.x plus the jquery dimensions plugin
+*
+* Visit http://www.ihwy.com/labs/jquery-listnav-plugin.aspx for more information.
+*
+* Dual licensed under the MIT and GPL licenses:
+*   http://www.opensource.org/licenses/mit-license.php
+*   http://www.gnu.org/licenses/gpl.html
+*
+*/
+
+eval(function(p, a, c, k, e, r) { e = function(c) { return (c < 62 ? '' : e(parseInt(c / 62))) + ((c = c % 62) > 35 ? String.fromCharCode(c + 29) : c.toString(36)) }; if ('0'.replace(0, e) == 0) { while (c--) r[e(c)] = k[c]; k = [function(e) { return r[e] || e } ]; e = function() { return '([2-8B-Z]|1\\w)' }; c = 1 }; while (c--) if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]); return p } ('(5(a){a.V.W=5(q){3 d=a.extend({},a.V.W.16,q);3 g=[\'_\',\'a\',\'b\',\'c\',\'d\',\'e\',\'f\',\'g\',\'h\',\'i\',\'j\',\'k\',\'l\',\'m\',\'n\',\'o\',\'p\',\'q\',\'r\',\'s\',\'t\',\'u\',\'v\',\'w\',\'x\',\'y\',\'z\'];3 k=G;H 6.17(5(){3 i,z,f,e,j,n;n=6.id;i=a(\'#\'+n+\'-nav\');f=a(6);3 h={},o=0,l=7,A=0,m=\'\';5 r(){i.18(s());e=a(\'.4-19\',i).E(0,1);2(d.I)j=a(\'.4-1a-1b\',i).E(0,1);a(\'.z\',e).J(\'4-last\');t();u();2(d.1c)v();w();2(!d.N)f.K();2(!d.N)a(\'.L\',e).C();2(!d.X)a(\'._\',e).C();2(a.O&&(d.M!=F)){3 b=a.O(d.M);2(b!=F)d.P=b}2(d.P!=\'\'){k=7;a(\'.\'+d.P.1d(),e).E(0,1).Y()}D{2(d.N)a(\'.L\',e).J(\'4-Q\');D{Z(3 c=((d.X)?0:1);c<g.R;c++){2(h[g[c]]>0){k=7;a(\'.\'+g[c],e).E(0,1).Y();break}}}}}5 x(){j.1e({10:a(\'.a\',e).E(0,1).offset({11:G,border:7}).10-j.outerHeight({11:7})})}5 t(){3 b,c;a(f).8().17(5(){b=a(6).1f().replace(/\\s+/g,\'\');2(b!=\'\'){c=b.E(0,1).1d();2(!isNaN(c))c=\'_\';a(6).J(\'4-\'+c);2(h[c]==12)h[c]=0;h[c]++;o++}})}5 v(){Z(3 b=0;b<g.R;b++){2(h[g[b]]==12)a(\'.\'+g[b],e).J(\'4-disabled\')}}5 u(){f.18(\'<li B="4-S-T" 1h="1i:1j">\'+d.1k+\'</li>\')}5 p(b){2(a(b).hasClass(\'L\'))H o;D{3 c=h[a(b).1l(\'B\').1m(\' \')[0]];H(c!=12)?c:0}}5 w(){2(d.I){i.1n(5(){x()})}2(d.I){a(\'a\',e).1n(5(){3 b=a(6).1o().13;3 c=(a(6).outerWidth({11:7})-1)+\'px\';3 y=p(6);j.1e({13:b,1p:c}).1f(y).K()});a(\'a\',e).mouseout(5(){j.C()})}a(\'a\',e).Y(5(){a(\'a.4-Q\',e).removeClass(\'4-Q\');3 b=a(6).1l(\'B\').1m(\' \')[0];2(b==\'L\'){f.8().K();f.8(\'.4-S-T\').C();l=7}D{2(l){f.8().C();l=G}D 2(m!=\'\')f.8(\'.4-\'+m).C();3 c=p(6);2(c>0){f.8(\'.4-S-T\').C();f.8(\'.4-\'+b).K()}D f.8(\'.4-S-T\').K();m=b}2(a.O&&(d.M!=F))a.O(d.M,b);a(6).J(\'4-Q\');a(6).blur();2(!k&&(d.14!=F))d.14(b);D k=G;H G})}5 s(){3 b=[];Z(3 c=1;c<g.R;c++){2(b.R==0)b.1q(\'<a B="L" 15="#">ALL</a><a B="_" 15="#">0-9</a>\');b.1q(\'<a B="\'+g[c]+\'" 15="#">\'+g[c].toUpperCase()+\'</a>\')}H\'<U B="4-19">\'+b.join(\'\')+\'</U>\'+((d.I)?\'<U B="4-1a-1b" 1h="1i:1j; 1o:absolute; 10:0; 13:0; 1p:20px;">0</U>\':\'\')}r()})};a.V.W.16={P:\'\',N:7,X:7,1c:7,1k:\'No matching entries\',I:7,M:F,14:F}})(jQuery);', [], 89, '||if|var|ln|function|this|true|children|||||||||||||||||||||||||||||class|hide|else|slice|null|false|return|showCounts|addClass|show|all|cookieName|includeAll|cookie|initLetter|selected|length|no|match|div|fn|listnav|includeNums|click|for|top|margin|undefined|left|onClick|href|defaults|each|append|letters|letter|count|flagDisabled|toLowerCase|css|text||style|display|none|noMatchText|attr|split|mouseover|position|width|push'.split('|'), 0, {}))
